e2709e06a4a80ecfd5649f82ea899e3560224347
Faiz Bagwan <faizbagwan184@gmail.com>
2026-02-05 18:29:23 +0530

    1 day

---
+++ core/home/views.py
@@ -0,0 +1,31 @@
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+#def home_view(request):
+#    return HttpResponse("Welcome to the Home Page!")
+
+def home_view(request):
+
+    peoples = [
+        {"name": "Alice", "age": 30},
+        {"name": "Bob", "age": 25},
+        {"name": "Charlie", "age": 35},
+        {"name": "David", "age": 28},
+        {"name": "Eve", "age": 22},
+    ]
+
+ #   for people in peoples:
+  #      print(people)
+    text = """Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, facilis vel? Laborum, a nemo nihil dolorem quia corporis facere hic, aspernatur quod, necessitatibus laudantium eligendi vitae aliquam repudiandae quo assumenda!"""
+
+
+    return render(request, 'index.html',context={ "peoples": peoples, "text": text})
+
+
+
+
+def success_page(request):
+    print("*" * 10)
+    return HttpResponse("Success! Your action was completed.")
