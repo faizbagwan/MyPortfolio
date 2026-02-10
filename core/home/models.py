b688b87ef3d4552c50f651326359f45b7fef8576
Faiz Bagwan <faizbagwan184@gmail.com>
2026-02-10 18:50:29 +0530

    7 days

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Student (models.Model):
+   #id = models. AutoField()
+   name = models.CharField(max_length=100)
+   age = models. IntegerField()
+   email = models. EmailField()
+   address = models. TextField()
+   image = models. ImageField()
+   file = models. FileField()
+
+class Product(models.Model):
+   pass
+
+class Contact(models.Model):
+   full_name = models.CharField()
+   email = models. EmailField()
+   phone = models.CharField(max_length=15)
+   email_sub = models.CharField(max_length=200)
+   message = models.TextField()
